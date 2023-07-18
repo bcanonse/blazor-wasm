da63580a22879eb0aa00bda2aed79222c578f7fe
Bryan Canon <bcanonse@gmail.com>
2023-07-18 12:30:04 -0600

    [ADD]Api url en appsettings

--- Program.cs
+++ Program.cs
@@ -6,6 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var apiUrl = builder.Configuration.GetValue<string>("ApiUrl");
+
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(apiUrl ?? "") });
 
 await builder.Build().RunAsync();
